adc1a0765e5555b349e319e3c28f91876a927d72
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-07-12 21:48:51 +0300

    Add files via upload

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grandmotherCount;  
+            int grandmotherTime = 10;    
+            int hourCount;         
+            int minutesCount;      
+            int timeWait;
+            int minutesPerHour = 60;
+
+
+            Console.Write("Введите кол-во старушек: ");
+            grandmotherCount = Convert.ToInt32(Console.ReadLine());
+
+            timeWait = grandmotherCount * grandmotherTime;
+            hourCount = timeWait / minutesPerHour;
+            minutesCount = timeWait % minutesPerHour;
+
+            Console.WriteLine("Вы должны отстоять в очереди " + hourCount + " часа и " + minutesCount + " минут.");
+        }
+    }
+}
